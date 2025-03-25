352fb994a78211a8fe6ac7fa4f4f744e3722036d
Athul <akkv.2507@gmail.com>
2025-03-25 15:10:53 +0530

    Added Create,Update and Delete services in vendor proto

--- vendor/vendor_grpc.pb.go
+++ vendor/vendor_grpc.pb.go
@@ -24,6 +24,9 @@ const (
 	VendorSevice_ApproveRejectCategory_FullMethodName = "/vendor.VendorSevice/ApproveRejectCategory"
 	VendorSevice_VendorProfile_FullMethodName         = "/vendor.VendorSevice/VendorProfile"
 	VendorSevice_UpdateProfile_FullMethodName         = "/vendor.VendorSevice/UpdateProfile"
+	VendorSevice_CreateService_FullMethodName         = "/vendor.VendorSevice/CreateService"
+	VendorSevice_UpdateService_FullMethodName         = "/vendor.VendorSevice/UpdateService"
+	VendorSevice_DeleteService_FullMethodName         = "/vendor.VendorSevice/DeleteService"
 )
 
 // VendorSeviceClient is the client API for VendorSevice service.
@@ -35,6 +38,9 @@ type VendorSeviceClient interface {
 	ApproveRejectCategory(ctx context.Context, in *ApproveRejectCategoryRequest, opts ...grpc.CallOption) (*ApproveRejectCategoryResponse, error)
 	VendorProfile(ctx context.Context, in *VendorProfileRequest, opts ...grpc.CallOption) (*VendorProfileResponse, error)
 	UpdateProfile(ctx context.Context, in *UpdateVendorProfileRequest, opts ...grpc.CallOption) (*UpdateVendorProfileResponse, error)
+	CreateService(ctx context.Context, in *CreateServiceRequest, opts ...grpc.CallOption) (*CreateServiceResponse, error)
+	UpdateService(ctx context.Context, in *UpdateServiceRequest, opts ...grpc.CallOption) (*UpdateServiceResponse, error)
+	DeleteService(ctx context.Context, in *DeleteServiceRequest, opts ...grpc.CallOption) (*DeleteServiceResponse, error)
 }
 
 type vendorSeviceClient struct {
@@ -95,6 +101,36 @@ func (c *vendorSeviceClient) UpdateProfile(ctx context.Context, in *UpdateVendor
 	return out, nil
 }
 
+func (c *vendorSeviceClient) CreateService(ctx context.Context, in *CreateServiceRequest, opts ...grpc.CallOption) (*CreateServiceResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(CreateServiceResponse)
+	err := c.cc.Invoke(ctx, VendorSevice_CreateService_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *vendorSeviceClient) UpdateService(ctx context.Context, in *UpdateServiceRequest, opts ...grpc.CallOption) (*UpdateServiceResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(UpdateServiceResponse)
+	err := c.cc.Invoke(ctx, VendorSevice_UpdateService_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
+func (c *vendorSeviceClient) DeleteService(ctx context.Context, in *DeleteServiceRequest, opts ...grpc.CallOption) (*DeleteServiceResponse, error) {
+	cOpts := append([]grpc.CallOption{grpc.StaticMethod()}, opts...)
+	out := new(DeleteServiceResponse)
+	err := c.cc.Invoke(ctx, VendorSevice_DeleteService_FullMethodName, in, out, cOpts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // VendorSeviceServer is the server API for VendorSevice service.
 // All implementations must embed UnimplementedVendorSeviceServer
 // for forward compatibility.
@@ -104,6 +140,9 @@ type VendorSeviceServer interface {
 	ApproveRejectCategory(context.Context, *ApproveRejectCategoryRequest) (*ApproveRejectCategoryResponse, error)
 	VendorProfile(context.Context, *VendorProfileRequest) (*VendorProfileResponse, error)
 	UpdateProfile(context.Context, *UpdateVendorProfileRequest) (*UpdateVendorProfileResponse, error)
+	CreateService(context.Context, *CreateServiceRequest) (*CreateServiceResponse, error)
+	UpdateService(context.Context, *UpdateServiceRequest) (*UpdateServiceResponse, error)
+	DeleteService(context.Context, *DeleteServiceRequest) (*DeleteServiceResponse, error)
 	mustEmbedUnimplementedVendorSeviceServer()
 }
 
@@ -128,6 +167,15 @@ func (UnimplementedVendorSeviceServer) VendorProfile(context.Context, *VendorPro
 }
 func (UnimplementedVendorSeviceServer) UpdateProfile(context.Context, *UpdateVendorProfileRequest) (*UpdateVendorProfileResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method UpdateProfile not implemented")
+}
+func (UnimplementedVendorSeviceServer) CreateService(context.Context, *CreateServiceRequest) (*CreateServiceResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method CreateService not implemented")
+}
+func (UnimplementedVendorSeviceServer) UpdateService(context.Context, *UpdateServiceRequest) (*UpdateServiceResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method UpdateService not implemented")
+}
+func (UnimplementedVendorSeviceServer) DeleteService(context.Context, *DeleteServiceRequest) (*DeleteServiceResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method DeleteService not implemented")
 }
 func (UnimplementedVendorSeviceServer) mustEmbedUnimplementedVendorSeviceServer() {}
 func (UnimplementedVendorSeviceServer) testEmbeddedByValue()                      {}
@@ -240,6 +288,60 @@ func _VendorSevice_UpdateProfile_Handler(srv interface{}, ctx context.Context, d
 	return interceptor(ctx, in, info, handler)
 }
 
+func _VendorSevice_CreateService_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(CreateServiceRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(VendorSeviceServer).CreateService(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: VendorSevice_CreateService_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(VendorSeviceServer).CreateService(ctx, req.(*CreateServiceRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _VendorSevice_UpdateService_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(UpdateServiceRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(VendorSeviceServer).UpdateService(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: VendorSevice_UpdateService_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(VendorSeviceServer).UpdateService(ctx, req.(*UpdateServiceRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
+func _VendorSevice_DeleteService_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(DeleteServiceRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(VendorSeviceServer).DeleteService(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: VendorSevice_DeleteService_FullMethodName,
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(VendorSeviceServer).DeleteService(ctx, req.(*DeleteServiceRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // VendorSevice_ServiceDesc is the grpc.ServiceDesc for VendorSevice service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -266,6 +368,18 @@ var VendorSevice_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "UpdateProfile",
 			Handler:    _VendorSevice_UpdateProfile_Handler,
+		},
+		{
+			MethodName: "CreateService",
+			Handler:    _VendorSevice_CreateService_Handler,
+		},
+		{
+			MethodName: "UpdateService",
+			Handler:    _VendorSevice_UpdateService_Handler,
+		},
+		{
+			MethodName: "DeleteService",
+			Handler:    _VendorSevice_DeleteService_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},
